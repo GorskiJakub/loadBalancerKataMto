f26c54e10359f875afe755611a1afe178b876c93
GorskiJakub <jakubg93@o2.pl>
2015-05-15 08:07:51 +0200

    test5 refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -5,17 +5,23 @@ public class ServerLoadBalancer {
 	public void balance(Server[] servers, Vm[] vms) {
 
 		for (Vm vm : vms) {
-			Server lessLoaded = null;
-			for (Server server : servers) {
-				if (lessLoaded == null
-						|| lessLoaded.currnetLoadPercentage > server.currnetLoadPercentage) {
-					lessLoaded = server;
-				}
 
-			}
+			Server lessLoaded = findLessLoadedServer(servers);
 			lessLoaded.addVm(vm);
 		}
 
+	}
+
+	private Server findLessLoadedServer(Server[] servers) {
+		Server lessLoaded = null;
+		for (Server server : servers) {
+			if (lessLoaded == null
+					|| lessLoaded.currnetLoadPercentage > server.currnetLoadPercentage) {
+				lessLoaded = server;
+			}
+
+		}
+		return lessLoaded;
 	}
 
 }
