f6d83d1c6c7bc3bd49ea8fb0aefc08a1d1c2036f
GorskiJakub <jakubg93@o2.pl>
2015-05-15 07:00:55 +0200

    TEST3 REFACTOR

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -4,6 +4,7 @@ import org.hamcrest.Matcher;
 
 public class Server {
 
+	private static final double MAXIMUM_LOAD = 100.0d;
 	private int capacity;
 	public double currnetLoadPercentage;
 
@@ -19,6 +20,12 @@ public class Server {
 	public int getCapacity() {
 		// TODO Auto-generated method stub
 		return capacity;
+	}
+
+	public void addVm(Vm vm) {
+		this.currnetLoadPercentage += (double)vm.getSize()
+				/ (double)this.getCapacity() *MAXIMUM_LOAD;
+		
 	}
 
 }
--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -4,8 +4,8 @@ public class ServerLoadBalancer {
 
 	public void balance(Server[] servers, Vm[] vms) {
 		if (vms.length > 0) {
-			servers[0].currnetLoadPercentage += (double)vms[0].getSize()
-					/ (double)servers[0].getCapacity() *100.0d;
+			servers[0].addVm(vms[0]);
+			
 		}
 
 	}
