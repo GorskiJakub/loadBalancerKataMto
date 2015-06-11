61472ae2acac4458ca5f3253784cb2a50a87439a
GorskiJakub <jakubg93@o2.pl>
2015-06-11 13:31:56 +0200

    test3 red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,6 +1,5 @@
 package edu.iis.mto.serverloadbalancer;
 
-
 import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentagematcher.hasLoadPercentageOf;
 import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
@@ -15,57 +14,54 @@ public class ServerLoadBalancerTest {
 	public void itCompiles() {
 		assertThat(true, equalTo(true));
 	}
-	
-	
+
 	@Test
-	public void balancingAServer_noVms_serverStaysEmpty(){
+	public void balancingAServer_noVms_serverStaysEmpty() {
 		Server theServer = a(server().withCapacity(1));
-		
-		balance(aListOfServersWith(theServer),anEmptyListOfVms());
-		
+
+		balance(aListOfServersWith(theServer), anEmptyListOfVms());
+
 		assertThat(theServer, hasLoadPercentageOf(0.0d));
 	}
-	
-		@Test
-		public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWiththeVm(){
-			Server theServer = a(server().withCapacity(1));
-			Vm theVm = a(vm().ofSize(1));
-			balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
-	
-			assertThat(theServer, hasLoadPercentageOf(100.0d));
-			assertThat("server should contains", theServer.contains(theVm));
-		}
+
+	@Test
+	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWiththeVm() {
+		Server theServer = a(server().withCapacity(1));
+		Vm theVm = a(vm().ofSize(1));
+		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+
+		assertThat(theServer, hasLoadPercentageOf(100.0d));
+		assertThat("server should contains", theServer.contains(theVm));
+	}
+
+	@Test
+	public void balancingOneServerWithTenSlotsCapacity_andOneSlotVm_fillTheServerWithTenPercent() {
+		Server theServer = a(server().withCapacity(10));
+		Vm theVm = a(vm().ofSize(1));
+		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+
+		assertThat(theServer, hasLoadPercentageOf(10.0d));
+		assertThat("server should contains", theServer.contains(theVm));
+	}
 
 	private <T> T a(Builder<T> builder) {
 		return builder.build();
 	}
 
 	private Vm[] aListOfVmsWith(Vm theVm) {
-			return new Vm[]{theVm};
-		}
-
-
-	private void balance(Server[] servers, Vm[] vms) {
-		 new ServerLoadBalancer().balance(servers,vms);
+		return new Vm[] { theVm };
 	}
 
+	private void balance(Server[] servers, Vm[] vms) {
+		new ServerLoadBalancer().balance(servers, vms);
+	}
 
 	private Vm[] anEmptyListOfVms() {
 		return new Vm[0];
 	}
 
-
 	private Server[] aListOfServersWith(Server theServer) {
-		return new Server[]{theServer};
+		return new Server[] { theServer };
 	}
-
-
-
-
-
-
-
-
-
 
 }
