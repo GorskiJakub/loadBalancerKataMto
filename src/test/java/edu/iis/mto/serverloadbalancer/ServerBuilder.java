987c6e25cbf73f5eebe99e2c03a342a0282d992d
GorskiJakub <jakubg93@o2.pl>
2015-06-11 21:00:38 +0200

    test5 green

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
@@ -1,8 +1,11 @@
 package edu.iis.mto.serverloadbalancer;
 
+import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
+
 public class ServerBuilder implements Builder<Server> {
 
 	private int capacity;
+	private double expectedLoad;
 
 	public ServerBuilder withCapacity(int capacity) {
 		this.capacity = capacity;
@@ -10,11 +13,21 @@ public class ServerBuilder implements Builder<Server> {
 	}
 
 	public Server build() {
-		return new Server(capacity);
+		Server server = new Server(capacity);
+		if(expectedLoad > 0){
+			int vmsSize = (int) (expectedLoad/ (double) server.getCapacity() * 100.0d);
+			server.addVm(vm().ofSize(vmsSize).build());
+		}
+		return server;
 	}
 	
 	public static ServerBuilder server() {
 		return new ServerBuilder();
+	}
+
+	public Builder<Server> withCurrentLoadOf(double expectedLoad) {
+		this.expectedLoad = expectedLoad;
+		return this;
 	}
 
 }
