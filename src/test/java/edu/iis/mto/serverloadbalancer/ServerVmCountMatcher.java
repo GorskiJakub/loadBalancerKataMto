eacb8986bd62a51cccbc675a573c4d1423b0377c
GorskiJakub <jakubg93@o2.pl>
2015-06-11 20:26:35 +0200

    test4 green

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerVmCountMatcher.java
@@ -0,0 +1,32 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class ServerVmCountMatcher extends TypeSafeMatcher<Server> {
+
+	private int exptectedCount;
+
+	public ServerVmCountMatcher(int exptectedCount) {
+		this.exptectedCount = exptectedCount;
+	}
+
+	public void describeTo(Description description) {
+		description.appendText("a server with vms count of ").appendValue(
+				exptectedCount);
+	}
+
+	@Override
+	protected void describeMismatchSafely(Server item,
+			Description description) {
+		description.appendText("a server with vms count of ").appendValue(
+				item.vmsCount());
+	}
+
+	@Override
+	protected boolean matchesSafely(Server item) {
+		return item.vmsCount() == exptectedCount;
+	}
+
+}
