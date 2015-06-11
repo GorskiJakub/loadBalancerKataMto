ab571f2eb011e54bbaa594755399e04ec6b3c8a3
GorskiJakub <jakubg93@o2.pl>
2015-06-11 18:39:48 +0200

    test2 green

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,6 +1,5 @@
 package edu.iis.mto.serverloadbalancer;
 
-
 import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasLoadPercentageOf;
 import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,49 +13,53 @@ public class ServerLoadBalancerTest {
 	public void itCompiles() {
 		assertThat(true, equalTo(true));
 	}
-	
-	
+
 	@Test
-	public void balancingAServer_noVms_serverStaysEmpty(){
+	public void balancingAServer_noVms_serverStaysEmpty() {
 		Server theServer = a(server().withCapacity(1));
-		
-		balance(aListOfServersWith(theServer),anEmptyListOfVms());
-		
+
+		balance(aListOfServersWith(theServer), anEmptyListOfVms());
+
 		assertThat(theServer, hasLoadPercentageOf(0.0d));
 	}
-	
-			@Test
+
+	@Test
 			public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWiththeVm(){
 				Server theServer = a(server().withCapacity(1));
 				Vm theVm = a(vm().ofSize(1));
-				balance(aListOfServersWith(theServer), aListOfVmsWith(theVm);
+				balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
 		
 				assertThat(theServer, hasLoadPercentageOf(100.0d));
 				assertThat("server should contains", theServer.contains(theVm));
 			}
 
+	private Vm[] aListOfVmsWith(Vm theVm) {
+		return new Vm[]{theVm};
+	}
+
+	private Vm a(VmBuilder builder) {
+		return builder.build();
+	}
+
+	private VmBuilder vm() {
+		return new VmBuilder();
+	}
 
 	private void balance(Server[] servers, Vm[] vms) {
 		new SeverLoadBalancer().balance(servers, vms);
-		
-	}
 
+	}
 
 	private Vm[] anEmptyListOfVms() {
 		return new Vm[0];
 	}
 
-
 	private Server[] aListOfServersWith(Server theServer) {
-		return new Server[]{theServer};
+		return new Server[] { theServer };
 	}
-
 
 	private Server a(ServerBuilder builder) {
 		return builder.build();
 	}
-
-
-
 
 }
