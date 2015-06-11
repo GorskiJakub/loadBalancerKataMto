2000b34ce1794db6964920571b231393f5687d96
GorskiJakub <jakubg93@o2.pl>
2015-06-11 22:16:56 +0200

    test3 green

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalance.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalance.java
@@ -3,10 +3,11 @@ package edu.iis.mto.serverloadbalancer;
 public class ServerLoadBalance {
 
 	public void balance(Server[] servers, Vm[] vms) {
-		if(vms.length >0){
-			servers[0].currentLoadPrcentage = 100.0d;
+		if (vms.length > 0) {
+			servers[0].currentLoadPrcentage += (double) vms[0].getSize()
+					/ (double) servers[0].getCapacity() * 100.0d;
 		}
-		
+
 	}
 
 }
