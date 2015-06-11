a875665a86bf39d6e3a45bff31d6f2833fec9579
GorskiJakub <jakubg93@o2.pl>
2015-06-11 19:21:11 +0200

    test3 refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -4,6 +4,7 @@ import org.hamcrest.Matcher;
 
 public class Server {
 
+	private static final double MAX_LOAD = 100.0d;
 	private int capacity;
 	public double currentLoadPercentage;
 
@@ -17,6 +18,12 @@ public class Server {
 
 	public double getCapacity() {
 		return capacity;
+	}
+
+	public void addVm(Vm vm) {
+		this.currentLoadPercentage += (double) vm.getSize()
+				/ (double) this.getCapacity() * MAX_LOAD;
+
 	}
 
 }
