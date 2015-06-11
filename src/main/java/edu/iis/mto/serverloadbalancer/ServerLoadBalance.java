db2f0129ff203095666956be49be608cf1ca22e6
GorskiJakub <jakubg93@o2.pl>
2015-06-11 22:34:05 +0200

    test5 refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalance.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalance.java
@@ -3,18 +3,22 @@ package edu.iis.mto.serverloadbalancer;
 public class ServerLoadBalance {
 
 	public void balance(Server[] servers, Vm[] vms) {
-			for(Vm vm: vms){
-				Server lessLoaded= null;
-				for(Server server: servers){
-					if(lessLoaded ==null || lessLoaded.currentLoadPrcentage>server.currentLoadPrcentage){
-						lessLoaded = server;
-					}
-				}
-				lessLoaded.addVm(vm);
-			}
-			
-			
+		for (Vm vm : vms) {
+			Server lessLoaded = findLessLoadedServer(servers);
+			lessLoaded.addVm(vm);
+		}
 
+	}
+
+	private Server findLessLoadedServer(Server[] servers) {
+		Server lessLoaded = null;
+		for (Server server : servers) {
+			if (lessLoaded == null
+					|| lessLoaded.currentLoadPrcentage > server.currentLoadPrcentage) {
+				lessLoaded = server;
+			}
+		}
+		return lessLoaded;
 	}
 
 }
