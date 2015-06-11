8b527873f625a8bcf56f5e126cc3f7f2f802e8bb
GorskiJakub <jakubg93@o2.pl>
2015-06-11 17:55:22 +0200

    test1 green

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -0,0 +1,13 @@
+package edu.iis.mto.serverloadbalancer;
+
+public class Server {
+
+	private int capacity;
+	public double currentLoadPercentage;
+
+	public Server(int capacity) {
+		this.capacity = capacity;
+	}
+
+
+}
---
+++ src/main/java/edu/iis/mto/serverloadbalancer/SeverLoadBalancer.java
@@ -0,0 +1,9 @@
+package edu.iis.mto.serverloadbalancer;
+
+public class SeverLoadBalancer {
+
+	public void balance(Server[] servers, Vm[] vms) {
+		
+	}
+
+}
