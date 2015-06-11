d7795ba3a2ac67f7f279b403903ddc5ca0cc49fd
GorskiJakub <jakubg93@o2.pl>
2015-06-11 20:46:47 +0200

    test4 refactor

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -2,6 +2,7 @@ package edu.iis.mto.serverloadbalancer;
 
 import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasLoadPercentageOf;
 import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
+import static edu.iis.mto.serverloadbalancer.ServerVmCountMatcher.hasAVmCountOf;
 import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -57,9 +58,6 @@ public class ServerLoadBalancerTest {
 		assertThat("server should contains", theServer.contains(theSecondVm));
 	}
 
-	private Matcher<? super Server> hasAVmCountOf(int exptectedCount) {
-		return new ServerVmCountMatcher(exptectedCount);
-	}
 
 	private Vm[] aListOfVmsWith(Vm... theVm) {
 		return theVm;
