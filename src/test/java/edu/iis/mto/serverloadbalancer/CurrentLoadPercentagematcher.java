a6c1c3f4b0a3e407801e044c10a06537ad524fbb
GorskiJakub <jakubg93@o2.pl>
2015-06-11 11:48:12 +0200

    test1 green

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentagematcher.java
@@ -0,0 +1,32 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class CurrentLoadPercentagematcher extends TypeSafeMatcher<Server> {
+
+	private double expectedLoadPercentage;
+
+	public CurrentLoadPercentagematcher(double expectedLoadPercentage) {
+		this.expectedLoadPercentage = expectedLoadPercentage;
+	}
+
+	public void describeTo(Description description) {
+		description.appendText("a server with load Percentage of ")
+				.appendValue(expectedLoadPercentage);
+	}
+
+	@Override
+	protected void describeMismatchSafely(Server item, Description description) {
+		description.appendText("a server with load Percentage of ")
+				.appendValue(item.currentLoadPercentage);
+	}
+
+	@Override
+	protected boolean matchesSafely(Server item) {
+		return expectedLoadPercentage == item.currentLoadPercentage
+				|| Math.abs(expectedLoadPercentage - item.currentLoadPercentage) < 0.01d;
+	}
+
+}
