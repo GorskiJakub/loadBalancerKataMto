cc01857761e40aa13c13aee215bacecf9a73de11
GorskiJakub <jakubg93@o2.pl>
2015-06-12 09:18:47 +0200

    test1 green

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -0,0 +1,12 @@
+package edu.iis.mto.serverloadbalancer;
+
+public class Server {
+
+	private int capacity;
+	public double currentLoadPErcentage;
+
+	public Server(int capacity) {
+		this.capacity = capacity;
+	}
+
+}
